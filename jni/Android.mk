fa2ff6b4315ad163efa6a0f6ab1d07afc2fd9e29
HAYASHI Kentaro <kenhys@gmail.com>
2011-08-18 12:42:15 +0900

    add JNI wrapper.
    original bzr status:
    revno: 3
    committer: HAYASHI Kentaro <kenhys@gmail.com>
    branch nick: zinnia-android
    timestamp: Thu 2011-08-18 12:42:15 +0900

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,14 +1,15 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-
 LOCAL_MODULE    := libzinnia
-#LOCAL_SRC_FILES := character.cpp feature.cpp libzinnia.cpp svm.cpp param.cpp
-#LOCAL_SRC_FILES := sexp.cpp
-#LOCAL_SRC_FILES := trainer.cpp
 LOCAL_SRC_FILES := character.cpp feature.cpp libzinnia.cpp param.cpp recognizer.cpp sexp.cpp svm.cpp trainer.cpp
-APP_STL := stlport_static
 LOCAL_CFLAGS    := -Werror -DHAVE_CONFIG_H
+include $(BUILD_STATIC_LIBRARY)
 
+# JNI
+include $(CLEAR_VARS)
+LOCAL_MODULE    := libzinniajni
+LOCAL_SRC_FILES := libzinniajni.cpp
+LOCAL_STATIC_LIBRARIES := libzinnia
 include $(BUILD_SHARED_LIBRARY)
 
---
+++ jni/libzinniajni.cpp
@@ -0,0 +1,407 @@
+#include "zinnia.h"
+#include <jni.h>
+#include "libzinniajni.h"
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_new
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1new
+  (JNIEnv *envenv, jobject jobj)
+{
+    zinnia_character_t* p = zinnia_character_new();
+    return reinterpret_cast<jlong>(p);
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_destroy
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1destroy
+(JNIEnv *env, jobject jobj, jlong character)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_set_value
+ * Signature: (JLjava/lang/String;)V
+ */
+JNIEXPORT void JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1set_1value
+(JNIEnv *env, jobject jobj, jlong character, jstring jstr)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_set_value2
+ * Signature: (JLjava/lang/String;J)V
+ */
+JNIEXPORT void JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1set_1value2
+(JNIEnv *env, jobject jobj, jlong character, jstring jstr, jlong length)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_value
+ * Signature: (J)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1value
+(JNIEnv *env, jobject jobj, jlong character)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_set_width
+ * Signature: (JJ)V
+ */
+JNIEXPORT void JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1set_1width
+(JNIEnv *env, jobject jobj, jlong character, jlong width)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_set_height
+ * Signature: (JJ)V
+ */
+JNIEXPORT void JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1set_1height
+(JNIEnv *env, jobject jobj, jlong character, jlong height)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_width
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1width
+(JNIEnv *env, jobject jobj, jlong character)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_height
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1height
+(JNIEnv *env, jobject jobj, jlong character)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_clear
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1clear
+(JNIEnv *env, jobject jobj, jlong stroke)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_add
+ * Signature: (JJII)I
+ */
+JNIEXPORT jint JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1add
+(JNIEnv *env, jobject jobj, jlong character, jlong id, jint x, jint y)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_strokes_size
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1strokes_1size
+(JNIEnv *env, jobject jobj, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_stroke_size
+ * Signature: (JJ)J
+ */
+JNIEXPORT jlong JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1stroke_1size
+(JNIEnv *env, jobject jobj, jlong, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_x
+ * Signature: (JJJ)I
+ */
+JNIEXPORT jint JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1x
+(JNIEnv *env, jobject jobj, jlong, jlong, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_y
+ * Signature: (JJJ)I
+ */
+JNIEXPORT jint JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1y
+(JNIEnv *env, jobject jobj, jlong, jlong, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_parse
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1parse
+  (JNIEnv *env, jobject jobj, jlong, jstring jstr)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_parse2
+ * Signature: (JLjava/lang/String;J)I
+ */
+JNIEXPORT jint JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1parse2
+(JNIEnv *env, jobject jobj, jlong, jstring jstr, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_to_string
+ * Signature: (JLjava/lang/String;J)I
+ */
+JNIEXPORT jint JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1to_1string
+(JNIEnv *env, jobject jobj, jlong, jstring jstr, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_character_strerror
+ * Signature: (J)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1character_1strerror
+(JNIEnv *env, jobject jobj, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_result_value
+ * Signature: (JJ)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1result_1value
+(JNIEnv *env, jobject jobj, jlong, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_result_score
+ * Signature: (JJ)F
+ */
+JNIEXPORT jfloat JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1result_1score
+(JNIEnv *env, jobject jobj, jlong, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_result_size
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1result_1size
+(JNIEnv *env, jobject jobj, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_result_destroy
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1result_1destroy
+(JNIEnv *env, jobject jobj, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_recognizer_new
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1recognizer_1new
+  (JNIEnv *env, jobject jobj)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_recognizer_destroy
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1recognizer_1destroy
+(JNIEnv *env, jobject jobj, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_recognizer_open
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1recognizer_1open
+  (JNIEnv *env, jobject jobj, jlong, jstring jstr)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_recognizer_open_from_ptr
+ * Signature: (JLjava/lang/String;J)I
+ */
+JNIEXPORT jint JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1recognizer_1open_1from_1ptr
+(JNIEnv *env, jobject jobj, jlong, jstring jstr, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_recognizer_close
+ * Signature: (J)I
+ */
+JNIEXPORT jint JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1recognizer_1close
+(JNIEnv *env, jobject jobj, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_recognizer_size
+ * Signature: (J)J
+ */
+JNIEXPORT jlong JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1recognizer_1size
+(JNIEnv *env, jobject jobj, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_recognizer_value
+ * Signature: (JJ)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1recognizer_1value
+(JNIEnv *env, jobject jobj, jlong, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_recognizer_strerror
+ * Signature: (J)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1recognizer_1strerror
+(JNIEnv *env, jobject jobj, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_recognizer_classify
+ * Signature: (JLjava/lang/String;J)J
+ */
+JNIEXPORT jlong JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1recognizer_1classify
+(JNIEnv *env, jobject jobj, jlong, jstring jstr, jlong jlongv)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_trainer_new
+ * Signature: ()J
+ */
+JNIEXPORT jlong JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1trainer_1new
+  (JNIEnv *env, jobject jobj)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_trainer_destroy
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1trainer_1destroy
+(JNIEnv *env, jobject jobj, jlong jlongv)
+{
+}
+   
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_trainer_add
+ * Signature: (JJ)I
+ */
+JNIEXPORT jint JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1trainer_1add
+(JNIEnv *env, jobject jobj, jlong trainer, jlong character)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_trainer_clear
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1trainer_1clear
+(JNIEnv *env, jobject jobj, jlong trainer)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_trainer_train
+ * Signature: (JLjava/lang/String;)I
+ */
+JNIEXPORT jint JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1trainer_1train
+  (JNIEnv *env, jobject jobj, jlong trainer, jstring filename)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_trainer_strerror
+ * Signature: (J)Ljava/lang/String;
+ */
+JNIEXPORT jstring JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1trainer_1strerror
+(JNIEnv *env, jobject jobj, jlong trainer)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_trainer_convert_model
+ * Signature: (Ljava/lang/String;Ljava/lang/String;D)I
+ */
+JNIEXPORT jint JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1trainer_1convert_1model
+  (JNIEnv *env, jobject jobj, jstring txt_model, jstring binary_mode, jdouble compression_threshold)
+{
+}
+
+/*
+ * Class:     org_xdump_android_zinnia_Zinnia
+ * Method:    zinnia_trainer_make_header
+ * Signature: (Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;D)I
+ */
+JNIEXPORT jint JNICALL Java_org_xdump_android_zinnia_Zinnia_zinnia_1trainer_1make_1header
+(JNIEnv *env, jobject jobj, jstring txt_model, jstring header_file, jstring name, jdouble compression_threashold)
+{
+}
+
