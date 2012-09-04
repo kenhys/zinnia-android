ddbc7c670ecd409816680a4895714e540a52d55d
HAYASHI Kentaro <kenhys@gmail.com>
2012-09-05 01:29:02 +0900

    add demo project

---
+++ demo/src/org/xdump/android/zinniademo/MainActivity.java
@@ -0,0 +1,22 @@
+package org.xdump.android.zinniademo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import org.xdump.android.zinnia.*;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
